7864b6a88d6d2a7fe5f9a273752db31d5d64da9b
sawan <dsawan.office8@gmail.com>
2023-09-09 18:32:40 +0530

    added code for onSuccess and onError

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -6,24 +6,40 @@ const fetchSuperHeroes = () => {
   return axios.get("http://localhost:4000/superheroes");
 };
 
+const onSuccess = (data) => {
+  console.log("Perform side effect after success", data);
+}
+
+const onError = (error) => {
+  console.log("Perform side effect after failure", error)
+}
+
 export const RQSuperHeroesPage = () => {
-  const { isLoading, isError, error, data, isFetching } = useQuery(
+  const { isLoading, isError, error, data, isFetching, refetch } = useQuery(
     "super-heroes",
     fetchSuperHeroes,
-    // { cacheTime: 5000 }   default value is 5 min
-    {staleTime: 30000}      // default value is 0 sec
+    {
+      // cacheTime: 5000 default value is 5 min
+      // staleTime: 30000, // default value is 0 sec
+      //   refetchOnMount: true,
+      // refetchOnWindowFocus: true,
+      enabled: false,
+      onSuccess,
+      onError
+    }
   );
 
   //By adding explicit staleTime, we will able to reduce network request
-  
-  console.log("status", { isLoading, isFetching });
+
+  //If there is refresh button, we want to show the loader, we can use `isFetching` for the same.
 
   if (isLoading) return <h2>Loading...</h2>;
   if (isError) return <h2>{error?.message}</h2>;
-
+  //For manually triggering the fetch, we use `refetch` function from useQuery.
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
+      <button onClick={refetch}>Fetch Heroes</button>
       {data?.data.map((hero) => {
         return <div key={hero?.id}>{hero.name}</div>;
       })}
